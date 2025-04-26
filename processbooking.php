dadf919a6d07af72d3f495ef30e76c088582d979
Remy-Amor <105914423@student.swin.edu.au>
2025-04-26 11:32:58 +1000

    added processbooking.php

---
+++ processbooking.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="description" content="Rohirrim Booking Form" >
+	<meta name="keywords"    content="MiddleEarth, Tours, Rohan" >
+	<meta name="author"      content="Grima Wormtongue" />
+     <title>Booking Confirmation</title>
+</head>
+<body>
+     <h1>Rohirrim Tour Booking Confirmation</h1>
+</body>
+</html>
