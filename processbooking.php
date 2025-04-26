959ad8cf3d198a89109ac62a3cffb957145cb9cb
Remy-Amor <105914423@student.swin.edu.au>
2025-04-26 11:45:55 +1000

    validating input

--- processbooking.php
+++ processbooking.php
@@ -10,5 +10,51 @@
 </head>
 <body>
      <h1>Rohirrim Tour Booking Confirmation</h1>
+     <?php
+          if ($_SERVER["REQUEST_METHOD"] == "POST") {
+               $firstname = clean_input($_POST["firstname"]);
+               $lastname = clean_input($_POST["lastname"]);
+               $age = clean_input($_POST["age"]);
+               $species = clean_input($_POST["species"]);
+               $accom = clean_input($_POST["accom"]);
+               $fourday = clean_input($_POST["4day"]);
+               $tenday = clean_input($_POST["10day"]);
+          
+               if (isset($firstname)) {
+                    echo "<h2>first name is ", $firstname, "</h2></br>";
+               }
+
+               if (isset($lastname)) {
+                    echo "<h2>last name is ", $lastname, "</h2></br>";
+               }
+
+               if (isset($age)) {
+                    echo "<h2>age is ", $age, "</h2></br>";
+               }
+
+               if (isset($species)) {
+                    echo "<h2>species is ", $species, "</h2></br>";
+               }
+
+               if (isset($accom)) {
+                    echo "<h2>you have selected accomodation</h2></br>";
+               }
+
+               if (isset($fourday)) {
+                    echo "<h2>you have selected a four day tour</h2></br>";
+               }
+
+               if (isset($tenday)) {
+                    echo "<h2>you have selected a ten day tour</h2></br>";
+               }
+          }
+
+  function clean_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+?> 
 </body>
 </html>
